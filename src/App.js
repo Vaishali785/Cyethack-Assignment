d971a2cfa676ade78821d40044f1a7e78b5b083c
Vaishali <aggarwal.vaishali785@gmail.com>
2024-06-20 20:19:17 +0530

    build: base files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import List from "./pages/List"
+import ListDetail from "./pages/ListDetail"
+import Dashboard from "./pages/Dashboard"
+import Login from "./pages/Login"
+import ProtectedRoute from "./components/ProtectedRoute"
+import ErrorPage from "./pages/ErrorPage"
 
+const routes = createBrowserRouter([
+	{
+		path: "/login",
+		element: <Login />,
+	},
+	{
+		element: <ProtectedRoute />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: "/list",
+				element: <List />,
+			},
+			{
+				path: "/list/details",
+				element: <ListDetail />,
+			},
+			{
+				path: "/dashboard",
+				element: <Dashboard />,
+			},
+		],
+	},
+])
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <RouterProvider router={routes}></RouterProvider>
 }
 
-export default App;
+export default App
